9ff0f887a6638f395e4bf3398489b044acf122ab
yida1207 <f263370@yahoo.com.tw>
2020-08-22 23:50:07 +0800

    Initial Commit

---
+++ SlotMachine-3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlotMachine-3
+//
+//  Created by Yida on 2020/8/22.
+//  Copyright © 2020 Yida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
